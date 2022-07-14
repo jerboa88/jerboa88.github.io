fb63a158ef56e293a680be674ec67acfbbb475a6
John Goodliff <johny112009@hotmail.com>
2022-07-13 20:25:53 -0600

    Add utility methods

--- src/common/utilities.ts
+++ src/common/utilities.ts
@@ -1,7 +1,34 @@
 import defaultProjectImage from '../images/default-tile-bg.png'
 
+
 // Exports
 
 export function getProjectImage(imageUrl: string) {
 	return imageUrl ? imageUrl : defaultProjectImage
+}
+
+// Default transition settings for Framer Motion animations
+export function getDefaultTransition(): any {
+	// TODO: Replace hardcoded boolean with `getIsMotionAllowed()`
+	if (true) {
+		return {
+			transition: {
+				type: 'spring',
+				stiffness: 350,
+				damping: 25,
+			}
+		}
+	} else {
+		return {
+			transition: {
+				duration: 0,
+			}
+		}
+	}
+}
+
+// Check if the window object exists
+// This will return false if the method is called from a server-side environment
+export function doesWindowExist(): boolean {
+	return typeof window !== 'undefined';
 }
