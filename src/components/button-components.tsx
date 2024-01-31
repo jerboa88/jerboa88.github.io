64eacf96eda53ae4205249268301bb1f7a24b3b8
John Goodliff <jerboa88@users.noreply.github.com>
2024-01-30 18:12:48 -0700

    Remove unused corner icon button component

--- src/components/button-components.tsx
+++ src/components/button-components.tsx
@@ -26,27 +26,3 @@ export function IconButton({ className = '', icon, disabled = false }: IconButto
 		</button>
 	);
 }
-
-
-interface CornerIconButtonPropsInterface {
-	className?: string;
-	direction: 'tl' | 'tr' | 'bl' | 'br';
-	icon: any;
-	disabled?: boolean
-}
-
-export function CornerIconButton({ className = '', direction, icon, disabled = false }: CornerIconButtonPropsInterface) {
-	const borderRadiusStyles = {
-		'tl': 'top-0 left-0 pt-6 pl-6 rounded-br-full bg-gradient-to-tl',
-		'tr': 'top-0 right-0 pt-6 pr-6 rounded-bl-full bg-gradient-to-tr',
-		'bl': 'bottom-0 left-0 pb-6 pl-6 rounded-tr-full bg-gradient-to-bl',
-		'br': 'bottom-0 right-0 pb-6 pr-6 rounded-tl-full bg-gradient-to-br'
-	}[direction];
-	const enabledStyles = disabled ? '' : 'hover:bg-white/20 transition-colors duration-200';
-
-	return (
-		<button className={`inline-block absolute p-10 ${borderRadiusStyles} from-transparent via-transparent to-black/50 ${enabledStyles} ${className}`}>
-			<FontAwesomeIcon icon={icon} className='block w-4 fa-lg fa-fw drop-shadow' />
-		</button>
-	);
-}
