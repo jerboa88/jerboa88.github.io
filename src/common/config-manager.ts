04a15ca47d5f7a611e5025122e7db6f828f7059e
John Goodliff <jerboa88@users.noreply.github.com>
2024-02-18 20:40:21 -0700

    Update site OpenGraph image alt text

--- src/common/config-manager.ts
+++ src/common/config-manager.ts
@@ -14,7 +14,7 @@ const config = {
 		description: 'A portfolio site for John Goodliff, a Software Developer based in Edmonton, Alberta.',
 		twitterUsername: 'jerboa88',
 		ogImagePath: 'images/og-image.png',
-		ogImageAltText: 'Sample OpenGraph cover image',
+		ogImageAltText: 'Cover image for johng.io',
 		siteUrl: 'https://johng.io/',
 		sourceUrl: 'https://github.com/jerboa88/jerboa88.github.io/',
 		linkedinUrl: 'https://www.linkedin.com/in/johngoodliff/',
