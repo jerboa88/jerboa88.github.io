cd4a3f58c1a982e6ec94f3512d463c3f3eb0f361
John Goodliff <jerboa88@users.noreply.github.com>
2024-03-04 19:19:21 -0700

    Move icon button component into buttons folder

--- src/components/buttons/icon-button.tsx
+++ src/components/buttons/icon-button.tsx
@@ -1,9 +1,13 @@
+/*
+	Icon button
+	-----------
+*/
+
+
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { IconDefinition } from '@fortawesome/fontawesome-svg-core';
 
-
-// Exports
 
 interface IconButtonPropsInterface {
 	className?: string;
