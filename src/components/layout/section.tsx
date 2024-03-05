0d3b9cfa353849c4028a0731ca9055f855446b6c
John Goodliff <jerboa88@users.noreply.github.com>
2024-03-04 18:49:30 -0700

    Remove custom styes for index page. Remove min-height for section component

--- src/components/layout/section.tsx
+++ src/components/layout/section.tsx
@@ -16,7 +16,7 @@ interface SectionPropsInterface {
 }
 
 const Section = forwardRef(({ className = '', id, title, children }: SectionPropsInterface, ref: ForwardedRef<HTMLElement>) => (
-	<section id={id} ref={ref} className={`flex flex-col w-full sm:w-5/6 xl:w-4/6 min-h-screen p-8 text-white text-center justify-center ${className}`}>
+	<section id={id} ref={ref} className={`flex flex-col w-full sm:w-5/6 xl:w-4/6 p-8 text-white text-center justify-center ${className}`}>
 		{title && <H2>{title}</H2>}
 		{children}
 	</section>
