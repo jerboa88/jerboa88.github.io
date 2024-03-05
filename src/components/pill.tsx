f3eac6fe566333191c97307cbd7686e41f1d1c76
John Goodliff <jerboa88@users.noreply.github.com>
2024-03-04 19:16:43 -0700

    Rename Pills component to Pill

--- src/components/pill.tsx
+++ src/components/pill.tsx
@@ -5,7 +5,7 @@
 
 
 import React from 'react';
-import { P, Span } from '../components/text-components';
+import { Span } from '../components/text-components';
 
 
 interface PillPropsInterface {
@@ -14,7 +14,7 @@ interface PillPropsInterface {
 	color: string;
 }
 
-export default function Pills({ className = '', text, color }: PillPropsInterface) {
+export default function Pill({ className = '', text, color }: PillPropsInterface) {
 	return (
 		<div key={text} className={`inline-block px-3 py-2 rounded-lg drop-shadow-sm ${className}`} style={{ background: `${color}` }}>
 			<Span>{text}</Span>
