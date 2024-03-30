a9e1de4516099a0e3537452c2ef980440039dbce
John Goodliff <jerboa88@users.noreply.github.com>
2024-03-30 16:33:55 -0600

    Refactor Pill component to use colors from config instead of colors from GitHub response

--- src/components/pill.tsx
+++ src/components/pill.tsx
@@ -5,18 +5,21 @@
 
 
 import React from 'react';
+import ConfigManager from '../common/config-manager';
 import { Span } from '../components/text-components';
 
 
 interface PillPropsInterface {
 	className?: string;
 	text: string;
-	color: string;
 }
 
-export default function Pill({ className = '', text, color }: PillPropsInterface) {
+export default function Pill({ className = '', text }: PillPropsInterface) {
+	const configManager = new ConfigManager();
+	const color = configManager.getProjectTypeColor(text) || configManager.getRoleTypeColor(text) || 'bg-transparent';
+
 	return (
-		<div key={text} className={`inline-block px-3 py-2 rounded-lg drop-shadow-sm ${className}`} style={{ background: `${color}` }}>
+		<div key={text} className={`inline-block w- fit h-fit px-3 py-2 rounded-lg drop-shadow-sm ${color} ${className}`}>
 			<Span>{text}</Span>
 		</div>
 	);
