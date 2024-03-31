f8e647adf8a7d2b01672924afc8a2074ad4683e3
John Goodliff <jerboa88@users.noreply.github.com>
2024-03-31 15:34:00 -0600

    Add SectionHeader component

---
+++ src/components/layout/section-header.tsx
@@ -0,0 +1,33 @@
+/*
+	Header text and optional button link for a page section
+	-------------------------------------------------------
+*/
+
+
+import React, { PropsWithChildren } from 'react';
+import { H2 } from '../../components/text-components';
+import { IconDefinition } from '@fortawesome/free-solid-svg-icons';
+import ButtonLink from '../links/button-link';
+
+
+interface SectionHeaderPropsInterface extends PropsWithChildren {
+	className?: string;
+	title: string;
+	button?: {
+		text: string;
+		icon: IconDefinition;
+		to: string;
+	};
+}
+
+export default function SectionHeader({ className = '', title, button }: SectionHeaderPropsInterface) {
+	return (
+		<>
+			<div className="w-full flex justify-between items-baseline pt-8">
+				<H2 className="mx-0 p-0">{title}</H2>
+				{button && <ButtonLink {...button} flip />}
+			</div>
+			<div className='divider h-auto m-0 pb-8 opacity-100' />
+		</>
+	);
+}
