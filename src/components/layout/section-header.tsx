a01b09cf5658e84eb67fef41ae6223d8955584ec
John Goodliff <jerboa88@users.noreply.github.com>
2024-03-31 15:40:55 -0600

    Align section header buttons to baseline instead of center

--- src/components/layout/section-header.tsx
+++ src/components/layout/section-header.tsx
@@ -25,9 +25,9 @@ export default function SectionHeader({ className = '', title, button }: Section
 		<>
 			<div className="w-full flex justify-between items-baseline pt-8">
 				<H2 className="mx-0 p-0">{title}</H2>
-				{button && <ButtonLink {...button} flip />}
+				{button && <ButtonLink {...button} flip className="self-baseline" />}
 			</div>
-			<div className='divider h-auto m-0 pb-8 opacity-100' />
+			<div className="divider h-auto m-0 pb-8 opacity-100" />
 		</>
 	);
 }
