0d7735649d4ef5e8be7bffbbc4c2417f018637cd
John Goodliff <jerboa88@users.noreply.github.com>
2024-03-30 18:36:26 -0600

    Replace a with LinkWrapper in ProjectCard component. Fix text wrapping issue

--- src/components/project-card.tsx
+++ src/components/project-card.tsx
@@ -5,14 +5,15 @@
 
 
 import React from 'react';
-import { Link } from 'gatsby';
 import { faStar } from '@fortawesome/free-solid-svg-icons';
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import ConfigManager from '../common/config-manager';
 import { getProjectImage } from '../common/utilities';
 import { ProjectInfoInterface } from '../common/types';
 import { H3, P, Span } from '../components/text-components';
 import Pill from '../components/pill';
 import IconButton from '../components/buttons/icon-button';
+import LinkWrapper from './links/link-wrapper';
 
 
 interface ProjectCardPropsInterface {
@@ -21,17 +22,19 @@ interface ProjectCardPropsInterface {
 
 export default function ProjectCard({ repo }: ProjectCardPropsInterface) {
 	const showOnCardHoverStyles = 'opacity-0 group-hover:opacity-100 transition-opacity transition-200';
+	const configManager = new ConfigManager();
+	const projectTypeColor = configManager.getProjectTypeColor(repo.typeName);
 
 	return (
-		<a href={repo.githubUrl} role='presentation' className='flex flex-col items-start w-full h-full p-4 text-ellipsis text-left relative z-10 justify-between align-middle bg-base-200 border-2 border-base-content/10 rounded-2xl overflow-hidden group duration-500 hover:bg-[--card-bg] interactive-scale' style={{ '--card-bg': `${repo.typeColor}aa` }}>
+		<LinkWrapper to={repo.githubUrl} className={`flex flex-col items-start w-full h-full p-4 text-ellipsis text-left relative z-10 justify-between align-middle bg-base-200 border-2 border-base-content/10 rounded-2xl overflow-hidden group duration-500 interactive-scale hover:${projectTypeColor}/75`}>
 			<div className='flex flex-row justify-between items-center w-full'>
-				<Pill text={repo.typeName} color={repo.typeColor} />
+				<Pill text={repo.typeName} className={projectTypeColor} />
 				<IconButton icon={faGithub} className={`m-0 ${showOnCardHoverStyles}`} disabled />
 			</div>
 			<div className='flex flex-row justify-center w-full'>
 				{/* TODO: Put project icon here? */}
 				{/* <img src={getProjectImage(repo.imageUrl)}/> */}
-				<div className={`flex flex-col z-16 h-full p-8 justify-center rounded-2xl px-10`}>
+				<div className={`flex flex-col z-16 h-full p-8 justify-center text-wrap rounded-2xl px-10`}>
 					<H3 className="font-semibold">
 						{repo.name}
 					</H3>
@@ -43,7 +46,7 @@ export default function ProjectCard({ repo }: ProjectCardPropsInterface) {
 			<div className='flex flex-row justify-between items-center w-full'>
 				<div className='flex flex-row justify-start items-center gap-2'>
 					{
-						repo.languages.map(language => <Pill key={language.name} className={`border ${showOnCardHoverStyles}`} text={language.name} color='transparent' />)
+						repo.languages.map(language => <Pill key={language.name} className={`border ${showOnCardHoverStyles}`} text={language.name} />)
 					}
 				</div>
 				<div className='flex flex-row items-center'>
@@ -51,6 +54,6 @@ export default function ProjectCard({ repo }: ProjectCardPropsInterface) {
 					<Span className='mr-2 mt-0.5'>{repo.stargazers}</Span>
 				</div>
 			</div>
-		</a>
+		</LinkWrapper>
 	);
 }
