df8b916bae9c02b45733420784c8fc91c7a14d28
John Goodliff <jerboa88@users.noreply.github.com>
2024-03-30 18:37:56 -0600

    Remove color choosing logic from Pill component

--- src/components/pill.tsx
+++ src/components/pill.tsx
@@ -5,7 +5,6 @@
 
 
 import React from 'react';
-import ConfigManager from '../common/config-manager';
 import { Span } from '../components/text-components';
 
 
@@ -15,11 +14,8 @@ interface PillPropsInterface {
 }
 
 export default function Pill({ className = '', text }: PillPropsInterface) {
-	const configManager = new ConfigManager();
-	const color = configManager.getProjectTypeColor(text) || configManager.getRoleTypeColor(text) || 'bg-transparent';
-
 	return (
-		<div key={text} className={`inline-block w- fit h-fit px-3 py-2 rounded-lg drop-shadow-sm ${color} ${className}`}>
+		<div key={text} className={`inline-block w-fit h-fit px-3 py-2 rounded-lg drop-shadow-sm ${className}`}>
 			<Span>{text}</Span>
 		</div>
 	);
