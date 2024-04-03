3bcaeb07802a20bf5b0e5fb56682d4c4b2b20e20
John Goodliff <jerboa88@users.noreply.github.com>
2024-04-03 12:19:22 -0600

    Update theme colors

--- src/config/themes.tsx
+++ src/config/themes.tsx
@@ -9,35 +9,35 @@ import type { ThemesConfigInterface } from '../common/types';
 
 export default {
 	light: {
-		'primary-header': '#F4511E',		// MD Deep Orange 600
-		'primary': '#E64A19',						// MD Deep Orange 700
-		'secondary-header': '#1976D2',	// MD Blue 700
-		'secondary': '#1565C0',					// MD Blue 800
-		'accent': '#673AB7',						// MD Purple 500
+		'primary-header': '#0D9488',		// TW Teal 600
+		'primary': '#0F766E',						// TW Teal 700
+		'secondary-header': '#BE123C',	// TW Rose 700
+		'secondary': '#E11D48',					// TW Rose 600
+		'accent': '#D97706',						// TW Amber 600
 		'neutral': '#111111',
-		'info': '#0277BD',							// MD Light Blue 800
-		'success': '#558B2F',						// MD Light Green 800
-		'warning': '#FF6F00',						// MD Amber 800
-		'error': '#C62828',							// MD Red 800
+		'info': '#0369A1',							// TW Sky 700
+		'success': '#15803D',						// TW Green 700
+		'warning': '#A16207',						// TW Yellow 700
+		'error': '#B91C1C',							// TW Red 700
 		'base-100': 'hsl(0 0% 100%)',
 		'base-200': 'hsl(0 0% 98%)',
 		'base-300': 'hsl(0 0% 96%)',
 		'base-content': '#000000',
 	},
 	dark: {
-		'primary-header': '#FFC107',		// MD Amber 500
-		'primary': '#FFCA28',						// MD Amber 400
-		'secondary-header': '#E91E63',	// MD Pink 500
-		'secondary': '#F06292',					// MD Pink 300
-		'accent': '#26C6DA',						// MD Cyan 400
+		'primary-header': '#14B8A6',		// TW Teal 500
+		'primary': '#2DD4BF',						// TW Teal 400
+		'secondary-header': '#F43F5E',	// TW Rose 500
+		'secondary': '#FB7185',					// TW Rose 400
+		'accent': '#FBBF24',						// TW Amber 400
 		'neutral': '#111111',
-		'info': '#4FC3F7',							// MD Light Blue 300
-		'success': '#AED581',						// MD Light Green 300
-		'warning': '#FFD54F',						// MD Amber 300
-		'error': '#E57373',							// MD Red 300
+		'info': '#7DD3FC',							// TW Sky 300
+		'success': '#86EFAC',						// TW Green 300
+		'warning': '#FDE047',						// TW Yellow 300
+		'error': '#FCA5A5',							// TW Red 300
 		'base-100': '#151515',
 		'base-200': '#090909',
 		'base-300': '#030303',
-		'base-content': '#ffffff',
+		'base-content': '#FFFFFF',
 	},
 } as ThemesConfigInterface;
