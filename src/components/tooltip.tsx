8b63dbe4b19011b1893c7dda6f8e3bee4b72df64
John Goodliff <jerboa88@users.noreply.github.com>
2024-04-06 19:10:11 -0600

    Add Tooltip component

---
+++ src/components/tooltip.tsx
@@ -0,0 +1,21 @@
+/*
+	Reusable tooltip component
+	--------------------------
+*/
+
+
+import React, { PropsWithChildren } from 'react';
+
+
+interface TooltipPropsInterface extends PropsWithChildren {
+	className?: string;
+	text: string;
+}
+
+export default function Tooltip({ className, children, text }: TooltipPropsInterface) {
+	return (
+		<div className={`tooltip hover:before:delay-500 ${className}`} data-tip={text}>
+			{children}
+		</div>
+	);
+}
