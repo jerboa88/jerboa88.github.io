4457b83935d162c7af751251e07eee9cd7b1128f
John Goodliff <jerboa88@users.noreply.github.com>
2024-04-06 19:14:16 -0600

    Add tooltips to footer buttons

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -8,6 +8,7 @@ import React from 'react';
 import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
 import { SiteMetadataInterface } from '../../common/types';
 import ButtonLink from '../links/button-link';
+import Tooltip from '../tooltip';
 
 
 interface FooterPropsInterface {
@@ -17,16 +18,24 @@ interface FooterPropsInterface {
 export default function Footer({ siteMetadata }: FooterPropsInterface) {
 	return (
 		<footer className="w-full bg-base-200 z-20">
-			<div className='divider h-auto m-0 opacity-100' />
-			<div className='flex-row p-4 pb-4 items-center justify-between'>
-				<nav layout="position" className="tabs flex-row justify-center font-button uppercase">
-					<ButtonLink text='Source' to={siteMetadata.sourceUrl} />
+			<div className="divider h-auto m-0 opacity-100" />
+			<div className="flex-row p-4 pb-4 items-center justify-between">
+				<nav className="tabs flex-row justify-center font-button">
+					<Tooltip text="View the source code on GitHub" className="tooltip-right">
+						<ButtonLink text="Source" to={siteMetadata.sourceUrl} className="uppercase" />
+					</Tooltip>
 					{/* TODO: Add privacy policy */}
-					{/* <ButtonLink text='Privacy' to='#' /> */}
+					{/* <Tooltip text="View the privacy policy" className="tooltip-right">
+						<ButtonLink text='Privacy' to='#' className="uppercase"/>
+					</Tooltip> */}
 				</nav>
-				<nav className='flex flex-row justify-center'>
-					<ButtonLink to={siteMetadata.author.link.linkedin} icon={faLinkedin} />
-					<ButtonLink to={siteMetadata.author.link.github} icon={faGithub} />
+				<nav className="flex flex-row justify-center">
+					<Tooltip text="View my profile on LinkedIn" className="tooltip-left">
+						<ButtonLink to={siteMetadata.author.link.linkedin} icon={faLinkedin} />
+					</Tooltip>
+					<Tooltip text="View my profile on GitHub" className="tooltip-left">
+						<ButtonLink to={siteMetadata.author.link.github} icon={faGithub} />
+					</Tooltip>
 				</nav>
 			</div>
 		</footer>
