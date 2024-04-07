133c4068d6dcd727416d63c83821e408368a0e0d
John Goodliff <jerboa88@users.noreply.github.com>
2024-04-06 19:12:57 -0600

    Add tooltip to section header buttons

--- src/components/layout/section-header.tsx
+++ src/components/layout/section-header.tsx
@@ -5,17 +5,18 @@
 
 
 import React, { PropsWithChildren } from 'react';
-import { H2 } from '../../components/text-components';
 import { IconDefinition } from '@fortawesome/free-solid-svg-icons';
+import { H2 } from '../text-components';
 import ButtonLink from '../links/button-link';
+import Tooltip from '../tooltip';
 
 
 interface SectionHeaderPropsInterface extends PropsWithChildren {
 	className?: string;
 	title: string;
 	button?: {
-		text?: string;
-		icon?: IconDefinition;
+		text: string;
+		icon: IconDefinition;
 		to: string;
 	};
 }
@@ -23,9 +24,13 @@ interface SectionHeaderPropsInterface extends PropsWithChildren {
 export default function SectionHeader({ className = '', title, button }: SectionHeaderPropsInterface) {
 	return (
 		<>
-			<div className="w-full flex justify-between items-baseline pt-8">
+			<div className={`w-full flex justify-between items-baseline pt-8 ${className}`}>
 				<H2>{title}</H2>
-				{button && <ButtonLink {...button} responsive flip className="self-baseline" />}
+				{button && (
+					<Tooltip text={button.text} className="tooltip-left">
+						<ButtonLink {...button} responsive flip className="self-baseline" />
+					</Tooltip>
+				)}
 			</div>
 			<div className="divider h-auto m-0 pb-8 opacity-100" />
 		</>
