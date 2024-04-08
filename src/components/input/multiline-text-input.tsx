973a84ca478a25b86585c3707d7d7fc9d7edc3bf
John Goodliff <jerboa88@users.noreply.github.com>
2024-04-08 16:56:59 -0600

    Add 'required', 'placeholder', and 'rows' properties to MultilineTextInput

--- src/components/input/multiline-text-input.tsx
+++ src/components/input/multiline-text-input.tsx
@@ -15,14 +15,15 @@ interface MultilineTextInputPropsInterface {
 	label: string;
 	placeholder?: string;
 	rows?: number;
+	required?: boolean;
 	disabled?: boolean;
 }
 
-export default function MultilineTextInput({ className = '', labelClassName = '', inputClassName = '', name, label, placeholder = '', rows = 4, disabled = false }: MultilineTextInputPropsInterface) {
+export default function MultilineTextInput({ className = '', labelClassName = '', inputClassName = '', name, label, placeholder = '', rows = 4, required = false, disabled = false }: MultilineTextInputPropsInterface) {
 	return (
 		<label className={`form-control ${className}`}>
 			<span className={`label label-text ${labelClassName}`}>{label}</span>
-			<textarea name={name} disabled={disabled} rows={rows} className={`textarea textarea-bordered w-full bg-base-200 text-base ${inputClassName}`} />
+			<textarea name={name} placeholder={placeholder} rows={rows} required={required} disabled={disabled} className={`textarea textarea-bordered w-full bg-base-200 text-base ${inputClassName}`} />
 		</label>
 	);
 }
