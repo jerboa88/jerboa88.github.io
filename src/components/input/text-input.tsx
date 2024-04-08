dc180975e411f3ab76e353e6ba74164929a8d143
John Goodliff <jerboa88@users.noreply.github.com>
2024-04-08 16:46:56 -0600

    Add TextInput component

---
+++ src/components/input/text-input.tsx
@@ -0,0 +1,28 @@
+/*
+	Reusable input component for single line text
+	---------------------------------------------
+*/
+
+
+import React from 'react';
+
+
+interface TextInputPropsInterface {
+	className?: string;
+	labelClassName?: string;
+	inputClassName?: string;
+	name: string;
+	label: string;
+	type?: string;
+	placeholder?: string;
+	disabled?: boolean;
+}
+
+export default function TextInput({ className = '', labelClassName = '', inputClassName = '', name, label, type = 'text', placeholder = '', disabled = false }: TextInputPropsInterface) {
+	return (
+		<label className={`form-control ${className}`}>
+			<span className={`label label-text ${labelClassName}`}>{label}</span>
+			<input name={name} type={type} placeholder={placeholder} disabled={disabled} className={`input input-bordered w-full bg-base-200 ${inputClassName}`} />
+		</label>
+	);
+}
