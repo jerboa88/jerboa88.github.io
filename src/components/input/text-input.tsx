98a225f66d4ec0467fd70677775060749db1d62b
John Goodliff <jerboa88@users.noreply.github.com>
2024-04-08 16:56:01 -0600

    Add 'required' property to TextInput

--- src/components/input/text-input.tsx
+++ src/components/input/text-input.tsx
@@ -15,14 +15,15 @@ interface TextInputPropsInterface {
 	label: string;
 	type?: string;
 	placeholder?: string;
+	required?: boolean;
 	disabled?: boolean;
 }
 
-export default function TextInput({ className = '', labelClassName = '', inputClassName = '', name, label, type = 'text', placeholder = '', disabled = false }: TextInputPropsInterface) {
+export default function TextInput({ className = '', labelClassName = '', inputClassName = '', name, label, type = 'text', placeholder = '', required = false, disabled = false }: TextInputPropsInterface) {
 	return (
 		<label className={`form-control ${className}`}>
 			<span className={`label label-text ${labelClassName}`}>{label}</span>
-			<input name={name} type={type} placeholder={placeholder} disabled={disabled} className={`input input-bordered w-full bg-base-200 ${inputClassName}`} />
+			<input name={name} type={type} placeholder={placeholder} required={required} disabled={disabled} className={`input input-bordered w-full bg-base-200 ${inputClassName}`} />
 		</label>
 	);
 }
