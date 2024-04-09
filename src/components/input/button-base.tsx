0b76a2a80abab440d5949255816ab7ce83b0ac5e
John Goodliff <jerboa88@users.noreply.github.com>
2024-04-08 19:05:54 -0600

    Add ButtonBase component

---
+++ src/components/input/button-base.tsx
@@ -0,0 +1,29 @@
+/*
+	A template for a responsive button with optional text/icon.
+	This should not be used directly, but rather as a base for other button components.
+	-----------------------------------------------------------------------------------
+*/
+
+
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { ButtonInterface } from '../../common/types';
+
+
+export default function ButtonBase({ className = '', iconClassName = '', textClassName = '', disabled = false, responsive = false, flip = false, icon, text }: ButtonInterface) {
+	const buttonFlipStyles = flip ? 'flex-row-reverse' : '';
+	const buttonStyles = `flex flex-row justify-center items-center self-center gap-2 text-sm font-button uppercase ${buttonFlipStyles} ${className}`;
+
+	const iconOnlyStyles = text ? '' : 'text-xl';
+	const iconStyles = `fa-fw ${iconOnlyStyles} ${iconClassName}`;
+
+	const textResponsiveStyles = (responsive && icon) ? 'max-lg:hidden' : '';
+	const textStyles = `${textResponsiveStyles} ${textClassName} `;
+
+	return (
+		<button className={buttonStyles} disabled={disabled}>
+			{icon && <FontAwesomeIcon icon={icon} className={iconStyles} />}
+			{text && <span className={textStyles}>{text}</span>}
+		</button>
+	);
+}
