ec910850fb0113264cd467fda52220077e771d04
John Goodliff <jerboa88@users.noreply.github.com>
2024-04-08 19:08:50 -0600

    Refactor SolidButton to use ButtonBase component

--- src/components/input/solid-button.tsx
+++ src/components/input/solid-button.tsx
@@ -5,33 +5,15 @@
 
 
 import React from 'react';
-import { IconDefinition } from '@fortawesome/fontawesome-svg-core';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { Span } from '../text-components';
+import ButtonBase from './button-base';
+import { ButtonInterface } from '../../common/types';
 
 
-interface SolidButtonLinkPropsInterface {
-	className?: string;
-	iconClassName?: string;
-	textClassName?: string;
-	disabled?: boolean;
-	responsive?: boolean;
-	icon?: IconDefinition;
-	text?: string;
-	flip?: boolean;
-	key?: string;
-}
-
-export default function SolidButtonLink({ className = '', iconClassName = '', textClassName = '', disabled = false, responsive = false, flip = false, icon, text }: SolidButtonLinkPropsInterface) {
-	const enabledStyles = disabled ? '' : 'interactive-card';
-	const flipStyles = flip ? 'flex-row-reverse' : '';
-	const iconOnlyStyles = text ? '' : 'text-xl';
-	const responsiveTextStyles = (responsive && icon) ? 'max-lg:hidden' : '';
+export default function SolidButton({ className = '', iconClassName, textClassName, icon, text, disabled, responsive, flip }: ButtonInterface) {
+	const buttonEnabledStyles = disabled ? '' : 'interactive-card';
+	const buttonStyles = `px-4 sm:px-8 py-2 bg-base-200 border-2 border-base-content/10 rounded-lg shadow-md ${buttonEnabledStyles} ${className}`;
 
 	return (
-		<button className={`flex flex-row justify-center items-center self-center gap-2 px-4 sm:px-8 py-2 text-sm font-button uppercase bg-base-200 border-2 border-base-content/10 rounded-lg shadow-md ${enabledStyles} ${flipStyles} ${className}`}>
-			{icon && <FontAwesomeIcon icon={icon} className={`fa-fw ${iconOnlyStyles} ${iconClassName}`} />}
-			{text && <Span className={`${textClassName} ${responsiveTextStyles}`}>{text}</Span>}
-		</button>
+		<ButtonBase className={buttonStyles} {...{ iconClassName, textClassName, disabled, responsive, flip, icon, text }} />
 	);
 }
