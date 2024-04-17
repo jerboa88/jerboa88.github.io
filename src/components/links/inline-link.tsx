afa02c55c7f61d3e8126bf4d54e00d54805da3b6
John Goodliff <jerboa88@users.noreply.github.com>
2024-04-17 15:57:51 -0600

    Add InlineLink component

---
+++ src/components/links/inline-link.tsx
@@ -0,0 +1,22 @@
+/*
+	An inline link component
+	------------------------
+*/
+
+
+import React from 'react';
+import { LinkInterface, PropsWithClassname } from '../../common/types';
+import LinkWrapper from './link-wrapper';
+
+
+interface InlineLinkPropsInterface extends PropsWithClassname, LinkInterface {
+	text: string;
+}
+
+export default function InlineLink({ className, text, to, isInternal, rel }: InlineLinkPropsInterface) {
+	return (
+		<LinkWrapper {...{ text, to, isInternal, rel }} className={`font-semibold transition-colors border-b-[1px] border-primary hover:border-secondary ${className}`}>
+			{text}
+		</LinkWrapper>
+	);
+}
