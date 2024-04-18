732a26bdc28268ecdbea4a13c8c876d7c04e17e8
John Goodliff <jerboa88@users.noreply.github.com>
2024-04-17 20:13:16 -0600

    Add bottom padding to inline links

--- src/components/links/inline-link.tsx
+++ src/components/links/inline-link.tsx
@@ -15,7 +15,7 @@ interface InlineLinkPropsInterface extends PropsWithClassName, LinkInterface {
 
 export default function InlineLink({ className, text, to, isInternal, rel }: InlineLinkPropsInterface) {
 	return (
-		<LinkWrapper {...{ text, to, isInternal, rel }} className={`font-semibold transition-colors border-b-[1px] border-primary hover:border-secondary ${className}`}>
+		<LinkWrapper {...{ text, to, isInternal, rel }} className={`pb-0.5 font-semibold transition-colors border-b-[1px] border-primary hover:border-secondary ${className}`}>
 			{text}
 		</LinkWrapper>
 	);
--- src/components/text/article.tsx
+++ src/components/text/article.tsx
@@ -15,7 +15,7 @@ interface ArticlePropsInterface extends PropsWithClassName, PropsWithChildren {
 
 export function Article({ className = '', children, html }: ArticlePropsInterface) {
 	const styles = 'prose dark:prose-invert max-w-none';
-	const linkStyles = 'prose-a:no-underline [&:not(.anchor)]:prose-a:border-b-[1px] prose-a:transition-colors prose-a:font-semibold prose-a:border-primary hover:prose-a:border-secondary';
+	const linkStyles = 'prose-a:no-underline prose-a:pb-0.5 [&:not(.anchor)]:prose-a:border-b-[1px] prose-a:transition-colors prose-a:font-semibold prose-a:border-primary hover:prose-a:border-secondary';
 	const anchorStyles = '[&_.anchor_svg]:transition-colors [&_.anchor_svg]:target:prose-headings:visible [&_.anchor_svg]:target:prose-headings:fill-primary';
 
 	return (
