dd4bd65bf3b17a00ae0b1732369007af13140b4d
John Goodliff <jerboa88@users.noreply.github.com>
2024-04-17 18:14:02 -0600

    Update anchor link styles for Article component

--- src/components/text/article.tsx
+++ src/components/text/article.tsx
@@ -15,11 +15,11 @@ interface ArticlePropsInterface extends PropsWithClassname, PropsWithChildren {
 
 export function Article({ className = '', children, html }: ArticlePropsInterface) {
 	const generalStyles = 'prose dark:prose-invert max-w-none';
-	const headingStyles = 'prose-headings:rounded-lg target:prose-headings:after:content-[" #"] target:prose-headings:animate-highlight';
 	const linkStyles = 'prose-a:no-underline [&:not(.anchor)]:prose-a:border-b-[1px] prose-a:transition-colors prose-a:font-semibold prose-a:border-primary hover:prose-a:border-secondary';
+	const anchorStyles = '[&_.anchor_svg]:transition-colors [&_.anchor_svg]:target:prose-headings:visible [&_.anchor_svg]:target:prose-headings:fill-primary';
 
 	return (
-		<article dangerouslySetInnerHTML={html ? { __html: html } : undefined} className={`${generalStyles} ${headingStyles} ${linkStyles} ${className}`}>
+		<article dangerouslySetInnerHTML={html ? { __html: html } : undefined} className={`${generalStyles} ${linkStyles} ${anchorStyles} ${className}`}>
 			{children}
 		</article>
 	);
