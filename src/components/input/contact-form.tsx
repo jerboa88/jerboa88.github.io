6d8998cb04a0fed1f796bb1fb642fb720ed2836c
John Goodliff <jerboa88@users.noreply.github.com>
2024-04-25 15:08:57 -0600

    Add layout props to ContactForm children. Increase max form width

--- src/components/input/contact-form.tsx
+++ src/components/input/contact-form.tsx
@@ -5,16 +5,16 @@
 
 
 import React from 'react';
-import { PropsWithClassName, InputValidationOptions } from '../../common/types';
 import { useForm, SubmitHandler } from 'react-hook-form';
-import { faPaperPlane } from '@fortawesome/free-solid-svg-icons';
+import { LayoutGroup, motion } from 'framer-motion';
+import { PropsWithClassName, InputValidationOptions } from '../../common/types';
 import TextInput from './text-input';
 import MultilineTextInput from './multiline-text-input';
 import SolidButton from './solid-button';
 
 
 // Allowed fields for the contact form
-export interface ContactFormFieldsInterface {
+interface ContactFormFieldsInterface {
 	name: string;
 	email: string;
 	message: string;
@@ -57,13 +57,13 @@ export default function ContactForm({ className = '' }: PropsWithClassName) {
 	}
 
 	return (
-		// TODO: Remove this line
-		// <form action="https://submit-form.com/echo" method="post" onSubmit={handleSubmit(onSubmit)} className={`w-full max-w-lg p-0 sm:p-8 flex flex-col gap-4 ${className}`}>
-		<form method="post" onSubmit={handleSubmit(onSubmit)} className={`w-full max-w-lg p-0 sm:p-8 flex flex-col gap-4 ${className}`}>
-			<TextInput name="name" label="Name" register={register} errors={errors} validationOptions={validationOptions.name} className="w-full" />
-			<TextInput name="email" label="Email" register={register} errors={errors} inputOptions={inputOptions.email} validationOptions={validationOptions.email} className="w-full" />
-			<MultilineTextInput name="message" label="Message" register={register} errors={errors} validationOptions={validationOptions.message} className="w-full" />
-			<SolidButton type="submit" icon={faPaperPlane} text="Send" className="w-full mt-2" />
-		</form>
+		<motion.form layout method="post" onSubmit={handleSubmit(onSubmit)} className={`w-full max-w-xl p-0 sm:p-8 flex flex-col gap-4 ${className}`}>
+			<LayoutGroup>
+				<TextInput name="name" label="Name" register={register} errors={errors} validationOptions={validationOptions.name} className="w-full" layout="position" />
+				<TextInput name="email" label="Email" register={register} errors={errors} inputOptions={inputOptions.email} validationOptions={validationOptions.email} className="w-full" layout="position" />
+				<MultilineTextInput name="message" label="Message" register={register} errors={errors} validationOptions={validationOptions.message} className="w-full" layout="position" />
+				<SolidButton type="submit" className="w-full mt-2" layout="position" layoutRoot />
+			</LayoutGroup>
+		</motion.form>
 	);
 }
