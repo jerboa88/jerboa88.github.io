628104f0f8e59a71f36cb2d39741f9d13ac63a03
John Goodliff <jerboa88@users.noreply.github.com>
2024-04-25 15:27:27 -0600

    Add form state variable to ContactForm. Add busy state to button and disable fields while form is being submitted

--- src/components/input/contact-form.tsx
+++ src/components/input/contact-form.tsx
@@ -7,6 +7,7 @@
 import React from 'react';
 import { useForm, SubmitHandler } from 'react-hook-form';
 import { LayoutGroup, motion } from 'framer-motion';
+import { faCircleNotch, faPaperPlane } from '@fortawesome/free-solid-svg-icons';
 import { PropsWithClassName, InputValidationOptions } from '../../common/types';
 import TextInput from './text-input';
 import MultilineTextInput from './multiline-text-input';
@@ -29,6 +30,12 @@ const inputOptions = {
 	}
 };
 
+enum FormState {
+	Idle,
+	Busy,
+	Submitted,
+	Error,
+}
 
 // Allowed fields for the contact form
 interface ContactFormFieldsInterface {
@@ -39,21 +46,26 @@ interface ContactFormFieldsInterface {
 }
 
 export default function ContactForm({ className = '' }: PropsWithClassName) {
+	const [formState, setFormState] = React.useState<FormState>(FormState.Idle);
+
 	// Options passed to React Hook Form for input validation
 	const validationOptions = {
 		name: {
 			maxLength: 50,
 			required: true,
+			disabled: formState === FormState.Busy,
 		} as InputValidationOptions,
 		email: {
 			maxLength: 50,
 			pattern: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/i,
 			required: true,
+			disabled: formState === FormState.Busy,
 		} as InputValidationOptions,
 		message: {
 			minLength: 30,
 			maxLength: 3000,
 			required: true,
+			disabled: formState === FormState.Busy,
 		} as InputValidationOptions
 	};
 	const {
@@ -64,7 +76,18 @@ export default function ContactForm({ className = '' }: PropsWithClassName) {
 		handleSubmit,
 	} = useForm<ContactFormFieldsInterface>();
 
-	const onSubmit: SubmitHandler<ContactFormFieldsInterface> = data => {
+	const submitButtonProps = (formState === FormState.Busy) ? {
+		iconClassName: 'animate-spin',
+		icon: faCircleNotch,
+		disabled: true,
+	} : {
+		icon: faPaperPlane,
+		text: 'Send',
+	};
+
+	const onSubmit: SubmitHandler<ContactFormFieldsInterface> = async formData => {
+		setFormState(FormState.Busy);
+
 		// TODO: Implement form submission here
 	}
 
@@ -74,7 +97,7 @@ export default function ContactForm({ className = '' }: PropsWithClassName) {
 				<TextInput name="name" label="Name" register={register} errors={errors} validationOptions={validationOptions.name} className="w-full" layout="position" />
 				<TextInput name="email" label="Email" register={register} errors={errors} inputOptions={inputOptions.email} validationOptions={validationOptions.email} className="w-full" layout="position" />
 				<MultilineTextInput name="message" label="Message" register={register} errors={errors} validationOptions={validationOptions.message} className="w-full" layout="position" />
-				<SolidButton type="submit" className="w-full mt-2" layout="position" layoutRoot />
+				<SolidButton type="submit" className="w-full mt-2" {...submitButtonProps} layout="position" layoutRoot />
 				<Checkbox name="_gotcha" register={register} errors={errors} className="hidden" />
 			</LayoutGroup>
 		</motion.form>
