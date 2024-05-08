4c8ea4f47b08388de85ba80057fca8a67942bab4
John Goodliff <jerboa88@users.noreply.github.com>
2024-05-07 20:05:20 -0600

    Refactor Tooltip with getClassNameProps. Fix tooltip colors

--- src/components/tooltip.tsx
+++ src/components/tooltip.tsx
@@ -6,6 +6,7 @@
 
 import React, { PropsWithChildren } from 'react';
 import { PropsWithClassName } from '../common/types';
+import { getClassNameProps } from '../common/utilities';
 
 
 interface TooltipPropsInterface extends PropsWithClassName, PropsWithChildren {
@@ -13,8 +14,13 @@ interface TooltipPropsInterface extends PropsWithClassName, PropsWithChildren {
 }
 
 export default function Tooltip({ className, children, text }: TooltipPropsInterface) {
+	const classNameProps = getClassNameProps(
+		'mouse-only:tooltip mouse-only:[--tooltip-color:oklch(var(--b1)/1)] mouse-only:[--tooltip-text-color:oklch(var(--bc)/1)] hover:before:delay-1000 hover:after:delay-1000 hover:before:shadow-md',
+		className,
+	);
+
 	return (
-		<div className={`mouse-only:tooltip hover:before:delay-1000 hover:after:delay-1000 ${className}`} data-tip={text}>
+		<div data-tip={text} {...classNameProps}>
 			{children}
 		</div>
 	);
