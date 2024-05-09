9f695e6a7af1e5a75388dc80a2c1e425f5616f63
John Goodliff <jerboa88@users.noreply.github.com>
2024-05-08 19:45:33 -0600

    Refactor ProjectCard with getClassNameProps

--- src/components/project-card.tsx
+++ src/components/project-card.tsx
@@ -7,9 +7,9 @@
 import React from 'react';
 import { faStar } from '@fortawesome/free-solid-svg-icons';
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
-import ConfigManager from '../common/config-manager';
-import { getProjectImage } from '../common/utilities';
 import { ProjectInfoInterface } from '../common/types';
+import ConfigManager from '../common/config-manager';
+import { getClassNameProps, getProjectImage } from '../common/utilities';
 import SubsectionHeading from './text/subsection-heading';
 import LinkWrapper from './links/link-wrapper';
 import GhostButton from './input/ghost-button';
@@ -17,27 +17,35 @@ import Pill from './pill';
 import Card from './card';
 
 
+// Types
+
 interface ProjectCardPropsInterface {
 	repo: ProjectInfoInterface;
 }
 
+
+// Constants
+
+const SHOW_ON_CARD_HOVER_STYLES = 'opacity-0 mouse-only:group-hover:opacity-100 transition-opacity transition-200';
+
+
 export default function ProjectCard({ repo }: ProjectCardPropsInterface) {
-	const showOnCardHoverStyles = 'opacity-0 mouse-only:group-hover:opacity-100 transition-opacity transition-200';
-	const configManager = new ConfigManager();
-	const projectTypeColor = configManager.getProjectTypeColor(repo.typeName);
+	const ghostButtonClassNameProps = getClassNameProps('!p-0 m-0', SHOW_ON_CARD_HOVER_STYLES);
+	const pillClassNameProps = getClassNameProps('border', SHOW_ON_CARD_HOVER_STYLES);
+	const projectTypeColor = new ConfigManager().getProjectTypeColor(repo.typeName);
 
 	return (
 		<LinkWrapper to={repo.githubUrl}>
 			<Card>
-				<div className={`flex flex-col justify-between items-start p-6 align-middle size-full text-ellipsis group`}>
+				<div className="flex flex-col justify-between items-start p-6 align-middle size-full text-ellipsis group">
 					<div className="flex flex-row justify-between items-center pr-2 w-full">
 						<Pill text={repo.typeName} className={projectTypeColor} />
-						<GhostButton icon={faGithub} className={`!p-0 m-0 ${showOnCardHoverStyles}`} disabled />
+						<GhostButton icon={faGithub} {...ghostButtonClassNameProps} disabled />
 					</div>
 					<div className="flex flex-row justify-center w-full">
 						{/* TODO: Put project icon here? */}
 						{/* <img src={getProjectImage(repo.imageUrl)}/> */}
-						<div className={`flex flex-col justify-center p-8 px-10 h-full rounded-2xl z-16 text-wrap`}>
+						<div className="flex flex-col justify-center p-8 px-10 h-full rounded-2xl z-16 text-wrap">
 							<SubsectionHeading className="font-semibold">
 								{repo.name}
 							</SubsectionHeading>
@@ -49,7 +57,9 @@ export default function ProjectCard({ repo }: ProjectCardPropsInterface) {
 					<div className="flex flex-row justify-between items-center pr-2 w-full">
 						<div className="flex flex-row gap-2 justify-start items-center">
 							{
-								repo.languages.map(language => <Pill key={language.name} className={`border ${showOnCardHoverStyles}`} text={language.name} />)
+								repo.languages.map(({ name }) => (
+									<Pill key={name} text={name} {...pillClassNameProps} />
+								))
 							}
 						</div>
 						<GhostButton icon={faStar} text={repo.stargazers} className="!p-0" iconClassName="text-xl" textClassName="font-bold" disabled />
