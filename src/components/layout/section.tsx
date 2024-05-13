86ad8478068021341611e0fa39fe96a04da8b35a
John Goodliff <jerboa88@users.noreply.github.com>
2024-05-12 22:28:31 -0600

    Add responsive property to section component

--- src/components/layout/section.tsx
+++ src/components/layout/section.tsx
@@ -19,18 +19,23 @@ interface SectionPropsInterface extends PropsWithClassName, PropsWithChildren {
 		icon: IconDefinition;
 		to: string;
 	};
+	responsive?: boolean;
 }
 
-const Section = forwardRef(({ className = '', id, title, button, children }: SectionPropsInterface, ref: ForwardedRef<HTMLElement>) => {
+const Section = forwardRef(({ className = '', id, title, button, responsive = true, children }: SectionPropsInterface, ref: ForwardedRef<HTMLElement>) => {
 	const classNameProps = getClassNameProps(
-		'flex z-10 flex-col justify-center p-8 w-full max-w-5xl sm:w-10/12 lg:w-9/12 xl:w-8/12',
+		'flex z-10 flex-col justify-center p-8 w-full',
+		responsive && 'max-w-5xl sm:w-10/12 lg:w-9/12 xl:w-8/12',	// Adjust width based on screen size
 		className,
+	);
+	const sectionHeaderClassNameProps = getClassNameProps(
+		responsive && 'pt-10',	// Add padding to account for floating page header
 	);
 
 	return (
 		<section {...{ id, ref, ...classNameProps }}>
 			{title && (
-				<SectionHeader title={title} button={button} />
+				<SectionHeader title={title} button={button} {...sectionHeaderClassNameProps} />
 			)}
 			{children}
 		</section>
