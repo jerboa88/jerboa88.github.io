0a044bc048775899b0c12d5353dd86334d02d27d
John Goodliff <jerboa88@users.noreply.github.com>
2024-06-27 19:37:39 -0600

    Add support for undefined `startDate` and `endDate` to DateRange component

--- src/components/text/date-range.tsx
+++ src/components/text/date-range.tsx
@@ -7,25 +7,31 @@ import type { PropsWithClassName } from '../../common/types';
 import { getClassNameProps } from '../../common/utilities';
 
 interface Props extends PropsWithClassName {
-	startDate: Date;
-	endDate: Date;
+	startDate?: Date;
+	endDate?: Date;
 }
 
 export function DateRange({ className, startDate, endDate }: Props) {
 	const classNameProps = getClassNameProps('font-bold', className);
 
-	const startMonth = startDate.toLocaleString('default', { month: 'long' });
-	const endMonth = endDate.toLocaleString('default', { month: 'long' });
-	const startYear = startDate.getFullYear();
-	const endYear = endDate.getFullYear();
-	const startDateString = `${startMonth} ${startYear === endYear ? '' : startYear}`;
+	const endMonth = endDate?.toLocaleString('default', { month: 'long' });
+	const endYear = endDate?.getFullYear();
 	const endDateString = `${endMonth} ${endYear}`;
+
+	const startMonth = startDate?.toLocaleString('default', { month: 'long' });
+	const startYear = startDate?.getFullYear();
+	const startDateString = `${startMonth} ${startYear === endYear ? '' : startYear}`;
 
 	return (
 		<div {...classNameProps}>
-			<time dateTime={startDate.toISOString()}>{startDateString}</time>
-			<span> - </span>
-			<time dateTime={endDate.toISOString()}>{endDateString}</time>
+			{startDate && (
+				<>
+					<time dateTime={startDate.toISOString()}>{startDateString}</time>
+					<span> - </span>
+					{!endDate && <time dateTime={new Date().toISOString()}>Present</time>}
+				</>
+			)}
+			{endDate && <time dateTime={endDate.toISOString()}>{endDateString}</time>}
 		</div>
 	);
 }
