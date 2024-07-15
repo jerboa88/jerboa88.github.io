5301520b060685025c2574c6be5a2c8c74fc222f
John Goodliff <jerboa88@users.noreply.github.com>
2024-07-14 20:32:13 -0600

    Add ResumeRoleEntries component

---
+++ src/components/resume-role-entries.tsx
@@ -0,0 +1,36 @@
+/*
+	A collection of resume entries for roles
+	----------------------------------------
+*/
+
+import type { Role } from '../common/types';
+import { ResumeEntry } from './resume-entry';
+
+interface Props {
+	roles: Role[];
+}
+
+export function ResumeRoleEntries({ roles }: Props) {
+	return (
+		<div className="flex flex-col gap-4">
+			{roles.map((role) => {
+				const title =
+					`${role.title} ${role?.type ? `(${role.type})` : ''}` as typeof role.title;
+
+				return (
+					<ResumeEntry
+						key={role.title}
+						title={title}
+						tagline={role.company}
+						taglineUrl={role.companyUrl}
+						taglineTooltip="View company website"
+						bullets={role.bullets}
+						startDate={role.startDate}
+						endDate={role.endDate}
+						location={role.location}
+					/>
+				);
+			})}
+		</div>
+	);
+}
