a43b134034595a1678008ab46e715800d914a344
John Goodliff <jerboa88@users.noreply.github.com>
2024-08-05 15:47:35 -0600

    Update project card to work with different project categories

--- src/components/project-card.tsx
+++ src/components/project-card.tsx
@@ -4,9 +4,14 @@
 */
 
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
-import { faStar } from '@fortawesome/free-solid-svg-icons';
+import { faGlobe, faStar } from '@fortawesome/free-solid-svg-icons';
 import { PROJECTS_PATH } from '../common/constants';
-import { getClassNameProps } from '../common/utils';
+import {
+	type Project,
+	ProjectCategory,
+	type PropsWithClassName,
+} from '../common/types';
+import { assertUnreachable, getClassNameProps } from '../common/utils';
 import { Card } from './card';
 import { GhostButton } from './input/ghost-button';
 import { LinkWrapper } from './links/link-wrapper';
@@ -16,7 +21,7 @@ import { SubsectionHeading } from './text/subsection-heading';
 // Types
 
 interface Props {
-	repo: Queries.GithubRepo;
+	project: Project;
 }
 
 // Constants
@@ -24,14 +29,54 @@ interface Props {
 const SHOW_ON_CARD_HOVER_STYLES =
 	'opacity-0 mouse-only:group-hover:opacity-100 transition-opacity transition-200';
 
-export function ProjectCard({ repo }: Props) {
-	const githubButtonClassNameProps = getClassNameProps(
+// Functions
+
+function getViewSourceButton(
+	project: Project,
+	classNameProps: PropsWithClassName,
+) {
+	if (!project.url) {
+		return <></>;
+	}
+
+	switch (project.category) {
+		case ProjectCategory.GithubRepo:
+			return (
+				<GhostButton
+					icon={faGithub}
+					tooltipText={`View ${project.name} on GitHub`}
+					{...classNameProps}
+					disabled
+				/>
+			);
+
+		case ProjectCategory.Other:
+			return (
+				<GhostButton
+					icon={faGlobe}
+					tooltipText={'View project page'}
+					{...classNameProps}
+					disabled
+				/>
+			);
+
+		default:
+			assertUnreachable(project);
+	}
+}
+
+export function ProjectCard({ project }: Props) {
+	const viewSourceClassNameProps = getClassNameProps(
 		'!p-0 m-0',
 		SHOW_ON_CARD_HOVER_STYLES,
 	);
 	const languagePillClassNameProps = getClassNameProps(
 		'border',
 		SHOW_ON_CARD_HOVER_STYLES,
+	);
+	const viewSourceButton = getViewSourceButton(
+		project,
+		viewSourceClassNameProps,
 	);
 
 	// Double negation is used for stargazerCount because 0 is evaluated as true for some reason
@@ -40,27 +85,22 @@ export function ProjectCard({ repo }: Props) {
 			<div className="flex flex-col justify-between items-start p-6 align-middle size-full text-ellipsis group">
 				<div className="flex flex-row justify-between items-center pr-2 w-full">
 					<Pill
-						text={repo.type.name ?? 'Unknown'}
-						className={repo.type.color}
+						text={project.type.name ?? 'Unknown'}
+						className={project.type.color}
 					/>
-					<GhostButton
-						icon={faGithub}
-						tooltipText={`View ${repo.name} on GitHub`}
-						{...githubButtonClassNameProps}
-						disabled
-					/>
+					{viewSourceButton}
 				</div>
 				<div className="flex flex-row justify-center w-full">
 					<div className="flex flex-col justify-center gap-4 p-10 h-full rounded-2xl z-16 text-wrap">
 						<SubsectionHeading className="font-semibold">
-							{repo.name}
+							{project.name}
 						</SubsectionHeading>
-						<span className="text-wrap">{repo.description}</span>
+						<span className="text-wrap">{project.description}</span>
 					</div>
 				</div>
 				<div className="flex flex-row justify-between items-center pr-2 w-full">
 					<div className="flex flex-row gap-2 justify-start items-center">
-						{repo.languages.map((language) => (
+						{project.languages.map((language) => (
 							<Pill
 								key={language}
 								text={language}
@@ -68,10 +108,10 @@ export function ProjectCard({ repo }: Props) {
 							/>
 						))}
 					</div>
-					{!!repo.stargazerCount && (
+					{!!project.stargazerCount && (
 						<GhostButton
 							icon={faStar}
-							text={repo.stargazerCount}
+							text={project.stargazerCount}
 							className="!p-0"
 							iconClassName="text-xl"
 							textClassName="font-bold"
@@ -84,12 +124,12 @@ export function ProjectCard({ repo }: Props) {
 		</Card>
 	);
 
-	if (!repo.url) {
+	if (!project.url) {
 		return cardElement;
 	}
 
 	return (
-		<LinkWrapper to={`${PROJECTS_PATH}/${repo.slug}`} isInternal>
+		<LinkWrapper to={`${PROJECTS_PATH}/${project.slug}`} isInternal>
 			{cardElement}
 		</LinkWrapper>
 	);
