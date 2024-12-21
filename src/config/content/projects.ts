95ed271ca6a81c3cce46f0b127d5aa42d19187e5
John Goodliff <jerboa88@users.noreply.github.com>
2024-12-20 17:37:57 -0700

    Rewrite projects config to match new types

--- src/config/content/projects.ts
+++ src/config/content/projects.ts
@@ -1,136 +1,16 @@
 /*
-	Custom rules for projects showcased on the site
-	-----------------------------------------------
+	Coding/design projects
+	----------------------
 */
 
-import { EntryVisibility } from '../../types/content/content.ts';
 import {
 	ProjectCategory,
 	type ProjectsConfig,
 } from '../../types/content/projects.ts';
 
-export const projectsConfig: ProjectsConfig = {
-	maxForPage: {
-		index: 10,
-		resume: 4,
-	},
-	projects: [
+export const PROJECTS_CONFIG: ProjectsConfig = {
+	[ProjectCategory.Other]: [
 		{
-			category: ProjectCategory.GithubRepo,
-			slug: '168421',
-			visibility: {
-				resume: EntryVisibility.Hide,
-			},
-		},
-		{
-			category: ProjectCategory.GithubRepo,
-			slug: 'compare-form-backends',
-			visibility: {
-				index: EntryVisibility.Show,
-			},
-		},
-		{
-			category: ProjectCategory.GithubRepo,
-			slug: 'custom-url-shortener',
-			visibility: {
-				index: EntryVisibility.Hide,
-				resume: EntryVisibility.Hide,
-			},
-		},
-		{
-			category: ProjectCategory.GithubRepo,
-			slug: 'dotfiles',
-			visibility: {
-				resume: EntryVisibility.Hide,
-			},
-		},
-		{
-			category: ProjectCategory.GithubRepo,
-			slug: 'game-maps',
-			visibility: {
-				resume: EntryVisibility.Hide,
-			},
-		},
-		{
-			category: ProjectCategory.GithubRepo,
-			slug: 'gimp-average-layers',
-			visibility: {
-				index: EntryVisibility.Show,
-			},
-		},
-		{
-			category: ProjectCategory.GithubRepo,
-			slug: 'hackathon',
-			visibility: {
-				index: EntryVisibility.Hide,
-				resume: EntryVisibility.Hide,
-			},
-		},
-		{
-			category: ProjectCategory.GithubRepo,
-			slug: 'image-signer',
-			visibility: {
-				index: EntryVisibility.Hide,
-				resume: EntryVisibility.Hide,
-			},
-		},
-		{
-			category: ProjectCategory.GithubRepo,
-			slug: 'jerboa88',
-			visibility: {
-				index: EntryVisibility.Hide,
-				resume: EntryVisibility.Hide,
-			},
-		},
-		{
-			category: ProjectCategory.GithubRepo,
-			slug: 'jerboa88-github-io',
-			visibility: {
-				index: EntryVisibility.Hide,
-				resume: EntryVisibility.Hide,
-			},
-		},
-		{
-			category: ProjectCategory.GithubRepo,
-			slug: 'make-me-laugh',
-			visibility: {
-				index: EntryVisibility.Hide,
-				resume: EntryVisibility.Hide,
-			},
-		},
-		{
-			category: ProjectCategory.GithubRepo,
-			slug: 'on-my-way',
-			visibility: {
-				index: EntryVisibility.Show,
-				resume: EntryVisibility.Pin,
-			},
-		},
-		{
-			category: ProjectCategory.GithubRepo,
-			slug: 'CMPUT301W20T24-H03/on-my-way',
-			visibility: {
-				index: EntryVisibility.Hide,
-				resume: EntryVisibility.Hide,
-			},
-		},
-		{
-			category: ProjectCategory.GithubRepo,
-			slug: 'rtg-for-outlook',
-			visibility: {
-				index: EntryVisibility.Hide,
-				resume: EntryVisibility.Hide,
-			},
-		},
-		{
-			category: ProjectCategory.GithubRepo,
-			slug: 'tinyi18n',
-			visibility: {
-				resume: EntryVisibility.Hide,
-			},
-		},
-		{
-			category: ProjectCategory.Other,
 			slug: 'pathfinder-v2',
 			name: 'Pathfinder v2',
 			description:
@@ -142,9 +22,6 @@ export const projectsConfig: ProjectsConfig = {
 			createdAt: '2023-01-17',
 			updatedAt: '2023-04-10',
 			url: 'https://cmput401.ca/projects/1a890021-cf4c-48d6-93c0-666ca1125373',
-			visibility: {
-				index: EntryVisibility.Hide,
-			},
 		},
 	],
-};
+} as const;
