4e67cb9eb7322eba395f8d90bd71fb66c1883346
John Goodliff <jerboa88@users.noreply.github.com>
2024-12-20 17:23:47 -0700

    Rewrite role manager using content manager utility functions

--- src/managers/content/roles.ts
+++ src/managers/content/roles.ts
@@ -4,7 +4,6 @@
 */
 
 import { rolesConfig } from '../../config/content/roles.ts';
-import type { PageContentEntryConfig } from '../../types/content/content.ts';
 import {
 	type EducationRole,
 	type EmploymentRole,
@@ -13,18 +12,19 @@ import {
 	RoleType,
 	type VolunteeringRole,
 } from '../../types/content/roles.ts';
-import { limit } from '../../utils/other.ts';
+import { keysOf } from '../../utils/other.ts';
 import { getPageContentConfig } from '../config.ts';
+import { filterEntries } from './utils.ts';
 
-/**
- * Given a config object for a single content entry, returns the max amount of items to display. If the limit is not specified, returns the maximum safe integer.
- *
- * @param pageEntryConfig The config object for a single content entry.
- * @returns The max amount of items to display.
- */
-function getEntriesLimit(pageEntryConfig: PageContentEntryConfig) {
-	return pageEntryConfig?.limit ?? Number.MAX_SAFE_INTEGER;
-}
+// Types
+
+type RoleSubsets = {
+	[RoleType.Employment]: EmploymentRole[];
+	[RoleType.Education]: EducationRole[];
+	[RoleType.Volunteering]: VolunteeringRole[];
+};
+
+// Functions
 
 /**
  * Converts a role config object to a role object.
@@ -38,9 +38,7 @@ function getEntriesLimit(pageEntryConfig: PageContentEntryConfig) {
  * @param roleConfig The role config object.
  * @returns The role object.
  */
-function convertRoleConfigToRole<T extends Role, U extends RoleConfig<T>>(
-	roleConfig: U,
-): T {
+function convertRoleConfigToRole<T extends Role>(roleConfig: RoleConfig<T>): T {
 	return {
 		title: roleConfig.title,
 		company: roleConfig.company,
@@ -53,51 +51,38 @@ function convertRoleConfigToRole<T extends Role, U extends RoleConfig<T>>(
 }
 
 /**
- * Returns a list of roles with formatted date objects
+ * Returns the role ID for a given role. The ID is its start date in the format 'YYYY-MM-DD'.
  *
- * @typeParam T - The type of role.
- * @typeParam U - The corresponding type of role config.
- * @param allRolesOfType The list of all roles of a type.
- * @param pageRolesConfig The page roles config object.
- * @returns The filtered list of roles of that type.
+ * @param role The role.
+ * @returns A string representing the role ID.
  */
-function getFilteredRolesOfType<T extends Role, U extends RoleConfig<T>>(
-	allRolesOfType: U[],
-	pageRolesConfig: PageContentEntryConfig,
-): T[] {
-	const pageSkillLimit = getEntriesLimit(pageRolesConfig);
-	const filteredRolesList = allRolesOfType
-		.map(convertRoleConfigToRole<T, U>)
-		.sort((a: T, b: T) => b.endDate.getTime() - a.endDate.getTime());
-
-	return limit(filteredRolesList, pageSkillLimit);
+function getRoleId<T extends Role>(role: T) {
+	return `${role.startDate.toISOString().slice(0, 10)}`;
 }
 
 /**
- * Given a page path, returns an object containing the computed list of roles for each role type.
+ * Given a page path, returns the computed list of roles for that page.
  *
  * @param pagePath The path of the page.
  * @returns An object containing the computed list of roles for each role type.
  */
 export function getRolesForPage(pagePath: string) {
-	const pageRolesConfig = getPageContentConfig(pagePath).roles;
-	const roleSubsets: {
-		[RoleType.Employment]: EmploymentRole[];
-		[RoleType.Education]: EducationRole[];
-		[RoleType.Volunteering]: VolunteeringRole[];
-	} = {
+	const pageContentConfig = getPageContentConfig(pagePath);
+	const pageRolesConfig = pageContentConfig?.roles;
+	const roleSubsets: RoleSubsets = {
 		[RoleType.Employment]: [],
 		[RoleType.Education]: [],
 		[RoleType.Volunteering]: [],
 	};
-
-	const roleTypes: RoleType[] = Object.keys(roleSubsets) as RoleType[];
+	const roleTypes = keysOf(roleSubsets);
 
 	// For each role type, get the computed list of roles
 	for (const roleType of roleTypes) {
-		roleSubsets[roleType] = getFilteredRolesOfType(
-			rolesConfig[roleType],
-			pageRolesConfig[roleType] ?? {},
+		roleSubsets[roleType] = filterEntries(
+			pagePath,
+			rolesConfig[roleType].map(convertRoleConfigToRole),
+			pageRolesConfig[roleType],
+			getRoleId,
 		);
 	}
 
