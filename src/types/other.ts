3afa8d778697ebe73d76ac92d358393092ebe4a5
John Goodliff <jerboa88@users.noreply.github.com>
2024-12-26 16:05:25 -0700

    Make Theme type more strict

--- src/types/other.ts
+++ src/types/other.ts
@@ -114,28 +114,28 @@ export enum ThemeType {
 /**
  * Colors for a single theme
  */
-export type Theme = Partial<{
+export type Theme = {
 	primary: string;
-	'primary-content': string;
 	secondary: string;
-	'secondary-content': string;
 	accent: string;
-	'accent-content': string;
 	neutral: string;
-	'neutral-content': string;
+	info: string;
+	success: string;
+	warning: string;
+	error: string;
 	'base-100': string;
 	'base-200': string;
 	'base-300': string;
 	'base-content': string;
-	info: string;
-	'info-content': string;
-	success: string;
-	'success-content': string;
-	warning: string;
-	'warning-content': string;
-	error: string;
-	'error-content': string;
-}>;
+	'primary-content'?: string;
+	'secondary-content'?: string;
+	'accent-content'?: string;
+	'neutral-content': string;
+	'info-content'?: string;
+	'success-content'?: string;
+	'warning-content'?: string;
+	'error-content'?: string;
+};
 
 /**
  * Config object used to define themes
