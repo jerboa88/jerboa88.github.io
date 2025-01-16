cc9d53efd80a0621691744bfde600ce5c65e2035
John Goodliff <jerboa88@users.noreply.github.com>
2025-01-15 18:54:08 -0700

    Add additional class projects

--- src/config/content/projects.ts
+++ src/config/content/projects.ts
@@ -23,5 +23,29 @@ export const PROJECTS_CONFIG: ProjectsConfig = {
 			updatedAt: '2023-04-10',
 			url: 'https://cmput401.ca/projects/1a890021-cf4c-48d6-93c0-666ca1125373',
 		},
+		{
+			slug: 'ridify',
+			name: 'Ridify',
+			description:
+				'A command line implementation of a ride sharing app written for a class project.',
+			exposition:
+				'A command line implementation of a ride sharing app written for a class project. Collaborated with teammates to integrate a command line interface with a SQL database via the SQLite library in Python.',
+			category: 'CLI App',
+			languages: ['Python', 'SQL'],
+			createdAt: '2018-11-01',
+			updatedAt: '2018-12-01',
+		},
+		{
+			slug: 'tes-forecaster-add-in',
+			name: 'TES Forecaster Add-in',
+			description:
+				'An Excel add-in that generates forecasts using Triple Exponential Smoothing (TES).',
+			exposition:
+				'A decision support system that generates forecasts of time-series data using triple exponential smoothing. Created as part of a class project with 3 other team members, this Excel add-in was written using Visual Basic for Applications (VBA) and the Solver library.',
+			category: 'Excel Add-in',
+			languages: ['VBA'],
+			createdAt: '2022-11-11',
+			updatedAt: '2022-12-11',
+		},
 	],
 } as const;
