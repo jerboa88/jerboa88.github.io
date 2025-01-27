8cee94487d2d67c1cc9b441dbe0268ea47c19ab9
John Goodliff <jerboa88@users.noreply.github.com>
2025-01-26 20:29:05 -0700

    Add Maybe utility type

--- src/types/utils.ts
+++ src/types/utils.ts
@@ -15,3 +15,11 @@ export type EmptyObject = Record<string, never>;
  * @returns A new object with the same keys as T, but with the types of U
  */
 export type Overwrite<T, U extends object> = Omit<T, keyof U> & U;
+
+/**
+ * Allow a type to be undefined
+ *
+ * @typeParam T - The base type
+ * @returns A new type that is either T or undefined
+ */
+export type Maybe<T> = T | undefined;
